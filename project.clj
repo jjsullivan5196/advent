2ef4a78921347e154d67325a58324238ace59497
John Sullivan <jsullivan@csumb.edu>
2021-05-05 10:03:28 -0700

    init

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject advent "0.1.0-SNAPSHOT"
+  :description "Advent of code playground"
+  :url "https://github.com/jjsullivan5196/advent"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [clojure.java-time "0.3.2"]
+                 [org.clojure/math.combinatorics "0.1.6"]
+                 [org.clojure/test.check "1.1.0"]])
